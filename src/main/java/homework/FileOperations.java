669faea224a0769f113ba366bba926a354b0fcd7
«Andrew» <gepatytich@gmail.com>
2024-10-19 23:15:47 +0700

    added the two homework

---
+++ src/main/java/homework/FileOperations.java
@@ -0,0 +1,13 @@
+package homework;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+interface FileOperations {
+    void saveToFile(FamilyTree familyTree, String fileName) throws
+            IOException;
+    FamilyTree loadFromFile(String fileName) throws IOException,
+            ClassNotFoundException;
+
+}
